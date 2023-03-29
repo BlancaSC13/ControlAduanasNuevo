af5db8e94c3f4bba2e9220e0d18dec7c9aaa9bad
BlancaSC13 <blanca.segura@ucr.ac.cr>
2023-03-29 11:47:52 -0600

    ControlAduanas Funcional

---
+++ src/main/java/cr/ac/ucr/paraiso/ie/progra2/lab2/Main.java
@@ -0,0 +1,5 @@
+package cr.ac.ucr.paraiso.ie.progra2.lab2;public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/cr/ac/ucr/paraiso/ie/progra2/lab2/model/Vehiculo.java
@@ -0,0 +1,2 @@
+package cr.ac.ucr.paraiso.ie.progra2.lab2.model;public class Vehiculo {
+}
